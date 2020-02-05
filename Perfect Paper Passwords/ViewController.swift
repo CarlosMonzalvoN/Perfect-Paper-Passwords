14dc968b1809820af70235e30d483cfdd2d94842
CarlosMonzalvoN <carlos.monzalvo.n@gmail.com>
2020-02-05 00:34:27 -0600

    implementation of algorithm PPP but is necessary separate on different classes

--- Perfect Paper Passwords/ViewController.swift
+++ Perfect Paper Passwords/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import CryptoKit
 
 class ViewController: UIViewController {
 
+    let alphabet = "! # % + 2  3 4 5 6 7 8 9 : = ? @ A B C D E F G H J K L M N P R S T U V W X Y Z a b c d e f g h i j k m n o p q r s t u v w x y z"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let alphabetArray = alphabet.split(separator: " ")
+        let key = SymmetricKey(size: .bits256)
+        //        let legibleSymmetrickey = key.withUnsafeBytes {
+        //            return Data(Array($0)) /// Asi lo usamos para mandarlo al keychain. Si lo queremos ver usamos b64
+        //        }
+        var pages : [String] = []
+        var counter : UInt128 = 0
+        
+        for index in 0...128 {
+            counter = UInt128(index)
+            let data = Data(bytes: &counter, count: MemoryLayout.size(ofValue: counter))
+            let encryptedContent = try! AES.GCM.seal(data, using: key).combined!
+            let intValueAes = encryptedContent.withUnsafeBytes { $0.load(as: UInt128.self) }
+            let residuo : Int = Int(intValueAes % 64)
+            pages.append(String(alphabetArray[residuo]))
+        }
+        print(pages)
     }
-
-
+    
 }
 
